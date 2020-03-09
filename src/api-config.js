1f5d1af3c8418d1b9e2d1ec068bc7dcda1be9abb
Jimil Patel <jimil.patel@jimilpatel-ltm1.internal.salesforce.com>
2020-03-08 18:55:21 -0700

    updated config for heroku dpeloyment

--- src/api-config.js
+++ src/api-config.js
@@ -1,5 +1,3 @@
 import runtimeEnv from '@mars/heroku-js-runtime-env';
 const env = runtimeEnv();
-export const API_ROOT = 'http://eda-demo-mule-relay.us-e2.cloudhub.io/api'
-
-//env.REACT_APP_EDM_RELAY_BACKEND_HOST || 'http://localhost:5000/api';
+export const API_ROOT = env.REACT_APP_EDM_RELAY_BACKEND_HOST || 'http://localhost:5000/api';
